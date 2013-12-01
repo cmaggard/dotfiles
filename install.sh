d4c186ab7d0e53983f8960c4ca11071e9d0d6f0c
Cody Maggard <codym@everfi.com>
2013-11-30 22:42:16 -0500

    iterm2 config

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 dotfiles="$HOME/.dotfiles"
-files="githelpers autotest gemrc gitconfig gitignore inputrc osx_specific powconfig rdebugrc rspec tmux.conf zshenv zshrc zshrc.zni"
+files="githelpers autotest gemrc gitconfig gitignore inputrc osx_specific powconfig rdebugrc rspec tmux.conf zshenv zshrc zshrc.zni iterm2"
 
 for file in $files; do
   rm $HOME/.$file
